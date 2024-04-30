16b17d8d318280a5472d3ad90abc57f8506230d8
mihailsamilov <m.shamilov@eapteka.ru>
2024-04-30 10:42:35 +0700

    gradle war

--- core/models/build.gradle.kts
+++ core/models/build.gradle.kts
@@ -8,6 +8,9 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+kotlin {
+    jvmToolchain(17)
+}
 
 dependencies {
     //immutable list
